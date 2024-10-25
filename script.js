eab2f69f19eb6f73132cdbc1b3cfa81d12f4b0a0
Richard Li <richard.li12@student.tdsb.on.ca>
2024-10-25 11:31:14 -0400

    finished herons form and started ambiguous

--- script.js
+++ script.js
@@ -1,14 +1,29 @@
 const herons = document.getElementById("herons");
 
-herons.addEventListener('click', () =>{
-    document.getElementById("result").value = heronsFormula
-})
+herons.addEventListener("submit", () =>{
+    document.getElementById("result").value = heronsFormula(
+        herons.elements['side-a'].value,
+        herons.elements['side-b'].value,
+        herons.elements['side-c'].value
+    )
+});
 
 const heronsFormula = (a,b,c) => {
-    (Math.sqrt(4 * (Math.pow(a, 2)) * Math.pow(b, 2) - Math.pow((Math.pow(a, 2) + Math.pow(b, 2) - 
+    
+    return (Math.sqrt(4 * (Math.pow(a, 2)) * Math.pow(b, 2) - Math.pow((Math.pow(a, 2) + Math.pow(b, 2) - 
     Math.pow(c, 2)), 2))) / 4
 }
 
-function steinberg2() {
-    
+const ambi = document.getElementById("ambi");
+
+ambi.addEventListener("submit", () =>{
+    document.getElementById("result").value = ambiFormula(
+        ambi.elements['Angle-A'].value,
+        ambi.elements['side-a'].value,
+        ambi.elements['side-b'].value
+    )
+});
+
+const ambiFormula = (ang,a,b) => {
+    if 
 }
