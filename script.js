ac4cc986e02d6206941690ab2377dfc31bbb9ec1
Richard Li <richard.li12@student.tdsb.on.ca>
2024-10-29 11:40:36 -0400

    added ambiguous case results

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const herons = document.getElementById("herons");
 
-herons.addEventListener("submit", () =>{
+herons.addEventListener("submit", () => {
     document.getElementById("result").value = heronsFormula(
         herons.elements['side-a'].value,
         herons.elements['side-b'].value,
@@ -8,22 +8,55 @@ herons.addEventListener("submit", () =>{
     )
 });
 
-const heronsFormula = (a,b,c) => {
-    
-    return (Math.sqrt(4 * (Math.pow(a, 2)) * Math.pow(b, 2) - Math.pow((Math.pow(a, 2) + Math.pow(b, 2) - 
-    Math.pow(c, 2)), 2))) / 4
+const heronsFormula = (a, b, c) => {
+
+    return (Math.sqrt(4 * (Math.pow(a, 2)) * Math.pow(b, 2) - Math.pow((Math.pow(a, 2) + Math.pow(b, 2) -
+        Math.pow(c, 2)), 2))) / 4
 }
 
 const ambi = document.getElementById("ambi");
 
-ambi.addEventListener("submit", () =>{
-    document.getElementById("result").value = ambiFormula(
-        ambi.elements['Angle-A'].value,
+ambi.addEventListener("submit", () => {
+    document.getElementById("result-2").value = ambiFormula(
+        ambi.elements['angle-a'].value,
         ambi.elements['side-a'].value,
         ambi.elements['side-b'].value
     )
 });
 
-const ambiFormula = (ang,a,b) => {
-    if 
+const ambiFormula = (ang, a, b) => {
+
+    const height = b * Math.sin(ang * (Math.PI / 180));
+
+    if (ang <= 90) {
+        if (a < height) {
+            return "no triangle";
+        }
+
+        else if (a == height) {
+            return "right triangle";
+        }
+
+        else if (a > b) {
+            return "one triangle";
+        }
+
+        else if (height < a && a < b) {
+            return "two triangles (ambiguous case)";
+        }
+    }
+
+    if (ang > 90) {
+        if (a < b || a == height) {
+            return "no triangle";
+        }
+
+        else if (a > b) {
+            return "one triangle";
+        }
+    }
+
+    else {
+        return "error";
+    }
 }
